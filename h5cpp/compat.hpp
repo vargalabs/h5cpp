339e48c2d025a1bd914740dd0e23931828b49147
steven varga <steven@vargaconsulting.ca>
2026-05-07 19:51:44 +0000

    [#85]:svarga:refactor, remove duplicate detection idiom shim
    Remove the unused h5::impl::compat detection-idiom copy and keep
    h5::meta::compat as the live C++17 compatibility surface.
    
    Also use the local void_t alias inside the detector specialization
    for internal consistency.
    
    No public h5::meta::compat API names are changed.
    H5meta.hpp and H5Tmeta.hpp are untouched.

--- h5cpp/compat.hpp
+++ h5cpp/compat.hpp
@@ -38,54 +38,6 @@ namespace h5::compat { // C++11 shim to lower from c++17
     }
 }
 
-namespace h5::impl::compat {
-    struct nonesuch {
-        nonesuch( ) = delete;
-        ~nonesuch( ) = delete;
-        nonesuch( nonesuch const& ) = delete;
-        void operator = ( nonesuch const& ) = delete;
-    };
-
-    template< class... > using void_t = void;
-    namespace detail {
-        template <class Default, class AlwaysVoid,
-                  template<class...> class Op, class... Args>
-        struct detector {
-          using value_t = std::false_type;
-          using type = Default;
-        };
-
-        template <class Default, template<class...> class Op, class... Args>
-        struct detector<Default, std::void_t<Op<Args...>>, Op, Args...> {
-          using value_t = std::true_type;
-          using type = Op<Args...>;
-        };
-    } // namespace detail
-
-    template <template<class...> class Op, class... Args>
-    using is_detected = typename detail::detector<nonesuch, void, Op, Args...>::value_t;
-
-    template <template<class...> class Op, class... Args>
-    using detected_t = typename detail::detector<nonesuch, void, Op, Args...>::type;
-
-    template <class Default, template<class...> class Op, class... Args>
-    using detected_or = detail::detector<Default, void, Op, Args...>;
-
-    // helper templates
-    template< template<class...> class Op, class... Args >
-    constexpr bool is_detected_v = is_detected<Op, Args...>::value;
-    template< class Default, template<class...> class Op, class... Args >
-    using detected_or_t = typename detected_or<Default, Op, Args...>::type;
-    template <class Expected, template<class...> class Op, class... Args>
-    using is_detected_exact = std::is_same<Expected, detected_t<Op, Args...>>;
-    template <class Expected, template<class...> class Op, class... Args>
-    constexpr bool is_detected_exact_v = is_detected_exact<Expected, Op, Args...>::value;
-    template <class To, template<class...> class Op, class... Args>
-    using is_detected_convertible = std::is_convertible<detected_t<Op, Args...>, To>;
-    template <class To, template<class...> class Op, class... Args>
-    constexpr bool is_detected_convertible_v = is_detected_convertible<To, Op, Args...>::value;
-}
-
 namespace h5::meta::compat {
 // N4436 and https://en.cppreference.com/w/cpp/experimental/is_detected 
     struct nonesuch {
@@ -105,7 +57,7 @@ namespace h5::meta::compat {
         };
 
         template <class Default, template<class...> class Op, class... Args>
-        struct detector<Default, std::void_t<Op<Args...>>, Op, Args...> {
+        struct detector<Default, void_t<Op<Args...>>, Op, Args...> {
           using value_t = std::true_type;
           using type = Op<Args...>;
         };
