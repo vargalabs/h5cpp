1e696c20a8a8d4a44c806534c950ece8a285ce01
steven varga <steven@vargaconsulting.ca>
2026-05-16 15:58:30 +0000

    [#226]:svarga:refactor, update open-exr half-float example to use vendored thirdparty/imath v3.2.2

--- examples/half-float/open-exr/utils.hpp
+++ examples/half-float/open-exr/utils.hpp
@@ -3,16 +3,17 @@
  * Author: Varga, Steven <steven@vargaconsulting.ca>
  */
 
+#include <half.h>
 #include <h5cpp/H5misc.hpp>
 
 namespace h5 { namespace utils {
 
-	template <> inline  std::vector<half> get_test_data( size_t n ){
+	template <> inline  std::vector<Imath::half> get_test_data( size_t n ){
 		std::random_device rd;
 		std::mt19937 eng(rd());
 		std::uniform_real_distribution<float> distr(-1e2, 1e2);
 
-		std::vector<half> vec (n);
+		std::vector<Imath::half> vec (n);
 		for(int i=0; i<n; i++ ) vec[i] = distr(eng);
 		return vec;
 	}
